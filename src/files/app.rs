0fc266ca46fa63119845ed705a965136bd71c2c4
Bowserinator <bowserinator@gmail.com>
2024-05-09 16:53:00 -0400

    Use path and randomized file ids instead of hash

--- src/files/app.rs
+++ src/files/app.rs
@@ -3,10 +3,11 @@ use actix_multipart::Multipart;
 use serde::{Deserialize, Serialize};
 
 use crate::files::postgres_handler::PostgresHandler;
-use crate::shared::types::app::{ErrorResponse, Response, login_fail};
+use crate::shared::types::app::{ErrorResponse, login_fail};
 
 use actix_identity::Identity;
 use actix_web::{get, post, web::{self, Data}, HttpRequest, HttpResponse, Result};
+use uuid::Uuid;
 
 #[derive(Serialize,Debug)]
 struct FileUploadResponse {
@@ -46,17 +47,14 @@ async fn create_file(
 
 #[derive(Deserialize,Debug)]
 struct SingleFileSearch {
-    id: String,
-    owner: String
+    id: Uuid
 }
 
 #[get("/v1/files/single")]
 async fn get_file(handler: Data<PostgresHandler>, identity: Option<Identity>, body: web::Query<SingleFileSearch>, req: HttpRequest) -> Result<HttpResponse> {
     if identity.is_some() {
         let json_data = body.into_inner();
-        let file_hash = json_data.id;
-        let user_id = json_data.owner;
-        let file_path = handler.file_exists(&user_id, &file_hash).await;
+        let file_path = handler.file_exists(&json_data.id).await;
 
         match file_path {
             Ok(file_path) => {
--- src/files/postgres_handler.rs
+++ src/files/postgres_handler.rs
@@ -1,4 +1,4 @@
-use std::{cmp, error::Error};
+use std::{cmp, error::Error, path::Path};
 
 use crate::shared::util::config::{self};
 
@@ -7,7 +7,6 @@ use futures::StreamExt;
 use serde::Serialize;
 use sqlx::Row;
 use sqlx::postgres::PgPool;
-use sha2::{Sha256, Digest};
 use tokio::io::AsyncWriteExt;
 use uuid::Uuid;
 
@@ -34,8 +33,8 @@ impl PostgresHandler {
 
 #[derive(Clone, Serialize)]
 pub struct FileResult {
+    id: Uuid,
     file_name: String,
-    hash: String,
     file_extension: String
 }
 
@@ -49,9 +48,7 @@ impl PostgresHandler {
     pub async fn init(&self) -> Result<(), sqlx::Error> {
         sqlx::query(r#"
         CREATE TABLE IF NOT EXISTS user_files (
-            file_hash text primary key unique CHECK(
-                length(file_hash) = 64 
-            ),
+            id uuid primary key unique,
             user_id text NOT NULL REFERENCES users(id),
             original_name text NOT NULL CHECK(length(original_name) < 2048),
             file_extension text NOT NULL CHECK(length(file_extension) < 5),
@@ -65,7 +62,7 @@ impl PostgresHandler {
     }
     pub async fn get_files(&self, user_id: &str, offset: u32, limit: u32) -> Result<Vec<FileResult>, sqlx::Error> {
         let mut files: Vec<FileResult> = Vec::new();
-        let mut query = sqlx::query("SELECT original_name, file_hash, file_extension FROM user_files WHERE user_id = $1 ORDER BY upload_date DESC OFFSET $2 LIMIT $3;")
+        let mut query = sqlx::query("SELECT id, original_name, file_extension FROM user_files WHERE user_id = $1 ORDER BY upload_date DESC OFFSET $2 LIMIT $3;")
             .bind(user_id)
             .bind(offset as i32)
             .bind(cmp::min(100, limit as i32))
@@ -74,8 +71,8 @@ impl PostgresHandler {
         while let Some(row) = query.next().await {
             let row = row?;
             files.push(FileResult {
-                file_name: row.get(0),
-                hash: row.get(1),
+                id: row.get(0),
+                file_name: row.get(1),
                 file_extension: row.get(2),
             });
         }
@@ -83,18 +80,14 @@ impl PostgresHandler {
         Ok(files)
     }
 
-    fn hash_file_name(file_name: &str) -> String {
-        format!("{:x}", Sha256::digest(file_name.as_bytes()))
-    }
+    pub async fn file_exists(&self, id: &Uuid) -> Result<String, sqlx::Error> {
+        let row = sqlx::query("SELECT user_id, file_extension FROM user_files WHERE id = $1;")
+            .bind(id)
+            .fetch_one(&self.pool).await?;
+        let file_extension = row.get::<String, &str>("file_extension");
+        let user_id = row.get::<String, &str>("user_id");
 
-    pub async fn file_exists(&self, user_id: &str, file_hash: &str) -> Result<String, sqlx::Error> {
-        let file_extension = sqlx::query("SELECT file_extension FROM user_files WHERE file_hash = $1 AND user_id = $2;")
-            .bind(file_hash)
-            .bind(user_id)
-            .fetch_one(&self.pool).await?
-            .get::<String, &str>("file_extension");
-
-        let file_path = format!("{}/{}{}.{}", self.user_uploads_dir, user_id, file_hash, file_extension);
+        let file_path = format!("{}/{}/{}.{}", self.user_uploads_dir, user_id, id.to_string(), file_extension);
         Ok(file_path)
     }
 
@@ -125,10 +118,16 @@ impl PostgresHandler {
              }};
         }
 
+        // Create subdirectory for user files
+        tokio::fs::create_dir_all(
+            Path::new(&self.user_uploads_dir).join(user_id)
+                .to_str().unwrap().to_string()
+            ).await.unwrap();
+
         let mut failed_files: Vec<u8> = Vec::new();
         let mut succeeded_files: Vec<String> = Vec::new();
-
         let mut count: u8 = 0;
+
         while let Some(item) = payload.next().await {
             if count == 50 {
                 while let Some(_) = payload.next().await {
@@ -139,7 +138,8 @@ impl PostgresHandler {
 
             let mut field = item?;
 
-            let current_path = format!("{}/{}", self.user_uploads_dir_tmp, Uuid::new_v4().to_string());
+            let current_path = Path::new(&self.user_uploads_dir_tmp).join(Uuid::new_v4().to_string())
+                .to_str().unwrap().to_string();
             let mut async_file = match tokio::fs::File::create(&current_path).await {
                 Ok(f) => f,
                 Err(_e) => {
@@ -151,6 +151,14 @@ impl PostgresHandler {
             let mut file_extension = String::new();
     
             if let Some(filename) = field.content_disposition().get_filename() {
+                // Note: this treats file extensions differently than say std::path
+                // Examples:
+                //   file.txt        -> file       .txt
+                //   .example.tar.gz -> .example   .tar.gz
+                //   file.tar.gz     -> file       .tar.gz
+                //   file            -> file       <empty>
+                //   .file           -> .file      <empty>
+
                 let parts = filename.split_once('.');
                 match parts {
                     Some(_) => { // File contained a ., such as test.tar.gz -> test and tar.gz
@@ -193,14 +201,17 @@ impl PostgresHandler {
             if errored_in_chunk { file_cleanup_and_continue!(&current_path, async_file, failed_files, count, tx); } // Already processed cleanup
             if file_name.is_empty() || file_extension.is_empty() { file_cleanup_and_continue!(&current_path, async_file, failed_files, count, tx); };
     
-            // copy the file to the user's uploads directory with the hashed filename
-            let file_hash = Self::hash_file_name(&file_name);
-            let file_path = format!("{}/{}{}.{}", self.user_uploads_dir, user_id, file_hash, file_extension);
-    
+            // copy the file to the user's uploads directory with the new filename
+            let file_id = Uuid::new_v4();
+            let file_path = Path::new(&self.user_uploads_dir)
+                .join(user_id)
+                .join(format!("{}.{}", file_id.to_string(), file_extension))
+                .to_str().unwrap().to_string();
+
             if async_file.shutdown().await.is_err() { file_cleanup_and_continue!(&current_path, async_file, failed_files, count, tx); }
 
-            let result = sqlx::query("INSERT INTO user_files (file_hash, user_id, original_name, file_extension, upload_date) VALUES ($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING;")
-                .bind(&file_hash)
+            let result = sqlx::query("INSERT INTO user_files (id, user_id, original_name, file_extension, upload_date) VALUES ($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING;")
+                .bind(&file_id)
                 .bind(user_id)
                 .bind(&file_name)
                 .bind(&file_extension)
@@ -211,8 +222,7 @@ impl PostgresHandler {
             if tokio::fs::rename(&current_path, &file_path).await.is_err() { file_cleanup_and_continue!(&current_path, async_file, failed_files, count, tx); }
 
             tx.commit().await?;
-            succeeded_files.push(file_path);
-
+            succeeded_files.push(file_id.to_string());
             count += 1;
         }
 
